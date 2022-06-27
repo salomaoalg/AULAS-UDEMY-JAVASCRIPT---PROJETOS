4dd530c3656d416408f561f84754840f5e1a3728
salomaoalg <sa.salomao@hotmail.com>
2022-06-27 20:54:21 -0300

    arrays e funções

---
+++ 05_arrays/index.js
@@ -0,0 +1,19 @@
+const alunos =['Maria','Luiz','Carla','Carlos','André'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+//console.log(alunos.slice(0, 4)); //[ 'Maria', 'Luiz', 'Carla', 'Carlos' ]
+//console.log(alunos)
+//delete alunos[1]; // [ 'Maria', <1 empty item>, 'Carla', 'Carlos', 'André' ]
+//const removido = alunos.pop();
+//console.log(removido) // Elemento removido
+//console.log(alunos) // Restante dos elementos
+//alunos.unshift('Josué'); // Coloca Elementos no começo do indice
+//alunos.push("Mariana"); // Coloca Elementos no FINAL do índice
+//alunos[alunos.length] = 'Leonice'; // Coloca Elementos no FINAL do índice
+//alunos[alunos.length] = 'José';
+//alunos[alunos.length] = 'Fábio';
+
+//console.log(alunos);
