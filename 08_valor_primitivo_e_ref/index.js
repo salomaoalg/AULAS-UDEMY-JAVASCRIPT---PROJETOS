61f1201f3f51594ee26b5b6b4d3f0f4af449ae5f
salomaoalg <sa.salomao@hotmail.com>
2022-07-29 19:21:05 -0300

    objeto funcionamento

--- 08_valor_primitivo_e_ref/index.js
+++ 08_valor_primitivo_e_ref/index.js
@@ -1,18 +1,31 @@
- /* 
- Primitivos (imutaveis) STRING, NUMBER, BOOLEAN, UNDEFINED 
- NULL (BIGINT, SYMBOL) - Valores copiados.
- */
+/* 
+Primitivos (imutaveis) STRING, NUMBER, BOOLEAN, UNDEFINED 
+NULL (BIGINT, SYMBOL) - Valores copiados.
+*/
 
- let a = [1,2,3];
- let b = [...a];
- let c = b;
+let a = [1, 2, 3];
+let b = [...a]; // Cópia os argumentos do array A sem apontar para o mesmo endereço da momoria
+let c = b; // Aponta para o mesmo endereço da momoria
 
- console.log(a,b);
- a.push(4);
- console.log(a,b);
+//console.log(a,b);
+//a.push(4);
+//console.log(a, b);
 
- b.pop();
- console.log(a.b);
+//b.pop();
+//console.log(a, b);
 
- a.push('Luiz');
- console.log(c);
+//a.push('Luiz');
+//console.log(c);
+
+const cadastro = {
+    nome: 'Maria',
+    sobrenome: 'joaquina',
+};
+
+const cadastro_copia = {...cadastro};
+
+cadastro.nome = 'Cirílo';
+cadastro.sobrenome = 'José';
+
+console.log(cadastro);
+console.log(cadastro_copia);
