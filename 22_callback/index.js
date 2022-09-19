9ca6ad7af16b7c450b846b5cddbd381d4a539d85
salomaoalg <sa.salomao@hotmail.com>
2022-09-19 20:55:29 -0300

    callback

---
+++ 22_callback/index.js
@@ -0,0 +1,45 @@
+function rand(min = 1000, max = 3000){
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num)
+}
+
+function f1(callback){
+    setTimeout(function(){
+        console.log('F1');
+        if(callback) callback();
+    }, rand())
+}
+function f2(callback){
+    setTimeout(function(){
+        console.log('F2');
+        if(callback) callback();
+    }, rand())
+}
+function f3(callback){
+    setTimeout(function(){
+        console.log('F3');
+        if(callback) callback();
+    }, rand())
+}
+
+/*f1(function(){
+    f2(function(){
+        f3(function(){
+
+        });
+    });
+});
+*/
+
+f1(f1callback)
+
+function f1callback(){
+    f2(f2callback)
+}
+function f2callback(){
+    f3(f3callback)
+}
+
+function f3callback(){
+    console.log('Olá Mundo')
+}
